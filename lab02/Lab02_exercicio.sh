810c267f3fe25b6c578f9b698a013b9d1dfec48e
Nara Costa <72310146+naracosta@users.noreply.github.com>
2024-10-16 22:16:37 -0300

    Create Lab02_exercicio.sh

---
+++ lab02/Lab02_exercicio.sh
@@ -0,0 +1,33 @@
+LED_GPIO_Y=16  # Usar uma variavel facilita alteracoes futuras na porta usada
+LED_GPIO_R=20
+LED_GPIO_G=21
+
+#  habilita
+echo $LED_GPIO_Y >> "/sys/class/gpio/export"
+echo $LED_GPIO_R >> "/sys/class/gpio/export"
+echo $LED_GPIO_G >> "/sys/class/gpio/export"
+
+i=0
+while [ $i -lt 5 ]; do
+    # acende o vermelho por 2s
+    echo 1 > "/sys/class/gpio/gpio$LED_GPIO_R/value"
+    sleep 2
+    echo 0 > "/sys/class/gpio/gpio$LED_GPIO_R/value"
+
+    # acende o verde por 1s
+    echo 1 > "/sys/class/gpio/gpio$LED_GPIO_G/value"
+    sleep 1
+    echo 0 > "/sys/class/gpio/gpio$LED_GPIO_G/value"
+
+    # acende o amarelo por 1s
+    echo 1 > "/sys/class/gpio/gpio$LED_GPIO_Y/value"
+    sleep 1
+    echo 0 > "/sys/class/gpio/gpio$LED_GPIO_Y/value"
+
+    i=$((i + 1))  # Incrementa o contador
+done
+
+# desabilita
+echo $LED_GPIO_Y >> "/sys/class/gpio/unexport"
+echo $LED_GPIO_R >> "/sys/class/gpio/unexport"
+echo $LED_GPIO_G >> "/sys/class/gpio/unexport"
